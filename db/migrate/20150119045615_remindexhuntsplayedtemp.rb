6354044987865ff8db381d7003f29791e9fe690b
parvathyv <paro_v@yahoo.com>
2015-01-19 13:41:48 -0500

    more styling

--- db/migrate/20150119045615_remindexhuntsplayedtemp.rb
+++
@@ -1,7 +0,0 @@
-class Remindexhuntsplayedtemp < ActiveRecord::Migration
-  def up
-    remove_index :huntsplayedusers, name: :index_huntsplayedusers_on_user_session_id_and_hunt_id
-  end
-  def down
-  end
-end

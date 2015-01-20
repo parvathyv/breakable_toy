70aa150967d9656af33a771a8d4f5af3b0b4ab69
parvathyv <paro_v@yahoo.com>
2015-01-19 20:17:57 -0500

    fixed question error, adding styling to maps

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -3,10 +3,16 @@ class TreesController < ApplicationController
   before_action :authenticate_user!
 
   def show
+    @content_array = []
     @hunt = Hunt.find(params[:id])
     @itinerary_array = @hunt.questionsets.first.uptoquestion(5)
     @places_array = @hunt.questionsets.order(:question_no)
-    @content_array = @hunt.questionsets.map{|question| "<img src="+"#{question.hunt_photo}><div>"+ question.description.to_s+"</div>"}
+    @hunt.questionsets.map do |question|
+      if question.description != nil
+       @content_array << "<img src="+"#{question.hunt_photo}><div>"+ question.description.to_s+"</div>"
+      end
+    end
+
   end
 
 
